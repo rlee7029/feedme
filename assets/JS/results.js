2a15ee5b35005479b364cd565c9da066629b0ad8
Kelly Li <kellee@Kellys-MacBook-Air-3.local>
2022-10-13 13:17:03 -0700

    renamed script.js to homepage.js for clarity

---
+++ assets/JS/results.js
@@ -0,0 +1,10 @@
+var app_ID = "a4759254";
+var app_Key = "8b51efe3abe8a26556f2211b458b449e";
+var listEl = document.getElementById("list");
+
+function getFoodName() {
+    
+}
+
+
+window.addEventListener("load", getFoodName);
